00d3d2fb33c5608b87130ba5e3e59345223e6023
dirk <dirk@maplelane.ag>
2025-04-28 16:28:27 -0400

    rearrange sections

---
+++ section-09-css-js/shared.js
@@ -0,0 +1,26 @@
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const planButton = document.querySelectorAll(".plan button");
+const cancelButton = document.querySelector(".modal__action--negative");
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+function showModalBackdrop() {
+  backdrop?.classList.add("open");
+  modal?.classList.add("open");
+  mobileNav?.classList.add("open");
+}
+
+function hideModalBackdrop() {
+  backdrop?.classList.remove("open");
+  modal?.classList.remove("open");
+  mobileNav?.classList.remove("open");
+}
+
+planButton.forEach((button) => {
+  button.addEventListener("click", showModalBackdrop);
+});
+toggleButton?.addEventListener("click", showModalBackdrop);
+
+backdrop?.addEventListener("click", hideModalBackdrop);
+cancelButton?.addEventListener("click", hideModalBackdrop);
