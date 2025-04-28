a031a7bcf9f9c07a71658e0dafb8a405b6739e53
dirk <dirk@maplelane.ag>
2025-04-28 16:24:52 -0400

    working with js wrap up

--- section-07-backgrounds-images/shared.js
+++ section-07-backgrounds-images/shared.js
@@ -6,24 +6,21 @@ const toggleButton = document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav");
 
 function showModalBackdrop() {
-  backdrop.classList.add("open");
-  modal.classList.add("open");
+  backdrop?.classList.add("open");
+  modal?.classList.add("open");
+  mobileNav?.classList.add("open");
 }
 
 function hideModalBackdrop() {
-  backdrop.classList.remove("open");
-  modal.classList.remove("open");
-  mobileNav.classList.remove("open");
+  backdrop?.classList.remove("open");
+  modal?.classList.remove("open");
+  mobileNav?.classList.remove("open");
 }
 
 planButton.forEach((button) => {
   button.addEventListener("click", showModalBackdrop);
 });
+toggleButton?.addEventListener("click", showModalBackdrop);
 
-backdrop.addEventListener("click", hideModalBackdrop);
-cancelButton.addEventListener("click", hideModalBackdrop);
-
-toggleButton.addEventListener("click", () => {
-  mobileNav.classList.add("open");
-  backdrop.classList.add("open");
-});
+backdrop?.addEventListener("click", hideModalBackdrop);
+cancelButton?.addEventListener("click", hideModalBackdrop);
